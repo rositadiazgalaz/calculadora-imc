88745c5ea39072e2fb5cb442441bbb1cd5bee1cb
genarvaez <ge_naravez@hotmail.com>
2017-05-08 23:31:30 -0300

    funcionalidades terminadas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-main.js
+
+function calculadora ()
+{
+	var resultado = document.getElementById("resultado");
+	var pesos = document.getElementById("pesos").value;
+	var altura = document.getElementById("alturas").value;
+	resultado.innerHTML = ( "SU IMC ES: " + pesos/Math.pow((altura/100),2).toFixed(2))
+}
